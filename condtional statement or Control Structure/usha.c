c69602cb43b14a156eba57b8a238ff5d26515f6c
Bibash Sharma <sharmabibash77@gmail.com>
2025-01-11 21:14:38 +0545

    sub

--- condtional statement or Control Structure/usha.c
+++ condtional statement or Control Structure/usha.c
@@ -61,7 +61,7 @@ int main()
 }
 */
 // Q.3 WAP to show hot day if temperature is greater than 30 degree celcius.
-#include <stdio.h>
+/*#include <stdio.h>
 int main()
 {
     int temp;
@@ -75,4 +75,76 @@ int main()
     {
         printf("Not specified");
     }
+}*/
+
+// WAP to display if your age is greater than 18 then you can vote else your age is below 18 then you can't vote.
+/*#include <stdio.h>
+
+int main() {
+    int vote;
+    printf("Enter your age: ");
+    scanf("%d", &vote);
+    if (vote > 18) {
+        printf("You can vote");
+    } else {
+        printf("You cannot vote");
+    }
+    getchar();
+    getchar();
+}
+*/
+// Check Pass or Fail Write a C program that takes a student's score as input and checks
+// if the score is 50 or higher. If the score is 50 or higher, print "You passed the exam."
+/*#include<stdio.h>
+int main()
+{
+    int score;
+    printf("enter score");
+    scanf("%d",&score);
+    if (score > 50)
+    {
+              printf("you passed the exam");
+              }else{
+                    printf("you fail the exam");
+                    }
+              getchar();
+              getchar();
+    }
+*/
+// 2.	Check Even or Odd Write a C program to check if a given integer is even. If it is even, print "The number is even."
+
+/*#include <stdio.h>
+int main()
+{
+    int num;
+    printf("Enter number to check odd or even ");
+    scanf("%d", &num);
+    if (num % 2 == 0)
+    {
+        printf("Even");
+    }
+    else
+    {
+        printf("Odd");
+    }
+}
+*/
+
+// WAP in C Take marks of 4 subject then find its total marks, percentage, if percentage is greater than 40 then display Passed in remarks , else failed.
+#include <stdio.h>
+int main()
+{
+    int eng, nep, math, sci, total;
+    float per;
+    printf("Marks of English : ");
+    scanf("%d", &eng);
+    printf("Marks of Nepali : ");
+    scanf("%d", &nep);
+    printf("Marks of Math : ");
+    scanf("%d", &math);
+    printf("Marks of Science : ");
+    scanf("%d", &sci);
+    total = eng + nep + math + sci;
+    per = (float)total / 4;
+    printf("%f", per);
 }
