63d8abf0772857b98117f13977bef511742d7f55
Bibash Sharma <sharmabibash77@gmail.com>
2025-01-23 17:09:26 +0545

    pointer

--- Functions/usha.c
+++ Functions/usha.c
@@ -83,3 +83,89 @@ int main()
     c = sum(a, b);
     printf("%d is add", c);
 }*/
+
+// Call by value
+/*
+a. in call by value, a copy of variable is passed to the function.
+b. changes made inside the function do not affect on original function.
+
+*/
+
+/*#include <stdio.h>
+void modifyValue(int x)
+{
+    x = x * 2;
+    printf("Inside function (call by value): %d\n", x);
+}
+int main()
+{
+    int num = 10;
+    printf("Before function call : %d\n", num);
+    modifyValue(num);
+    printf("After function call : %d\n", num);
+}*/
+
+// call by reference
+/*
+a. C doesn't support Call by reference directly. However, we can achieve this behavior by passing the address (pointer) of the variable to the function.
+b. Changes made inside the function affect the original function.
+*/
+
+// Declaration of pointer ----> int *x, int *y, char *a[10] etc
+
+/*#include <stdio.h>
+void modifyValue(int *x)
+{
+    *x = *x * 2;
+    printf("Inside function (call by reference) : %d\n", *x);
+}
+int main()
+{
+    int num = 10;
+    printf("Before function call : %d\n", num);
+    modifyValue(&num);
+    printf("After call function : %d\n", num);
+    return 0;
+}*/
+
+// WAP to swaping two varibales without using pointer and function.(call by value)
+/*#include <stdio.h>
+int main()
+{
+    int a, b;
+    printf("Enter first number : ");
+    scanf("%d", &a);
+    printf("Enter second number : ");
+    scanf("%d", &b);
+    printf("Before swap a=%d,b=%d\n", a, b);
+    swap(a, b);
+}
+void swap(int a, int b)
+{
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+    printf("After swap a=%d,b=%d\n", a, b);
+}*/
+
+// WAP to swaping two varibales using pointer and function. (call by reference)
+/*#include <stdio.h>
+void swap(int *a, int *b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+int main()
+{
+    int a, b;
+    printf("Enter first number : ");
+    scanf("%d", &a);
+    printf("Enter second number : ");
+    scanf("%d", &b);
+    printf("Before swap a=%d,b=%d", a, b);
+    swap(&a, &b);
+    printf("\nAfter swap a=%d,b=%d", a, b);
+}*/
